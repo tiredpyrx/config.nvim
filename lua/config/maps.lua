3734058d2192270c1616c2358e561dcf4a454e22
qqpe <qqkdev@gmail.com>
2024-10-20 22:58:37 +0300

    add no-arrow keymaps

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -4,3 +4,8 @@ vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
+-- no arrows
+vim.keymap.set("n", "<Up>", ":echo 'use k instead'<cr>")
+vim.keymap.set("n", "<Down>", ":echo 'use j instead'<cr>")
+vim.keymap.set("n", "<Left>", ":echo 'use h instead'<cr>")
+vim.keymap.set("n", "<Right>", ":echo 'use l instead'<cr>")
